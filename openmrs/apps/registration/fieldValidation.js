36efe004a67dae588eaa7f015c61f74518c2e7db
sphoplunkar <sphoplunkar@iplit.in>
2026-09-02 18:53:34 +0530

    Age not below 18 if patient type is self

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -5,6 +5,38 @@ Bahmni.Registration.customValidator = {
         },
         errorMessage: "REGISTRATION_AGE_ERROR_KEY"
     },
+    "age.years": {
+        method: function (name, value) {
+
+            var patientTypeElement = document.getElementById("Patient Type");
+
+            if (!patientTypeElement) {
+                return true;
+            }
+
+            var selectedOption =
+                patientTypeElement.options[patientTypeElement.selectedIndex];
+
+            var patientType = selectedOption
+                ? selectedOption.text.trim()
+                : "";
+
+            if (patientType === "Self") {
+
+                if (value !== undefined &&
+                    value !== null &&
+                    value !== "" &&
+                    parseInt(value, 10) < 18) {
+
+                    return false;
+                }
+            }
+
+            return true;
+        },
+
+        errorMessage: "Age should not be below 18"
+    },
     "Telephone Number": {
         method: function (name, value, personAttributeDetails) {
             return value && value.length> 6;
